38581f4bc27e62de7c360cc989217491ca14206e
OmkarKhilari <omkarkhilari95@gmail.com>
2024-01-06 23:23:35 +0530

    custom textfield made

--- lib/components/custom_textfield.dart
+++ lib/components/custom_textfield.dart
@@ -1,2 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
+class CustomTextfield extends StatelessWidget {
+  const CustomTextfield({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  double width = MediaQuery.of(context).size.width;
+
+    return Container(
+      width: width*0.75,
+      margin: EdgeInsets.only(top: 8),
+      decoration: BoxDecoration(
+        color: Color(0xff000000).withOpacity(0.6),
+        borderRadius: BorderRadius.circular(20)
+      ),
+      // padding: EdgeInsets.only(left: 12,right: 12),
+      child: TextField(
+        decoration: InputDecoration(
+          hintText: 'Message...',
+          hintStyle: GoogleFonts.montserrat(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            color: Colors.grey
+          ),
+          icon: IconButton(
+            onPressed: () {}, 
+            icon: Icon(Icons.add),
+            color: Colors.grey,
+            ),
+          contentPadding: EdgeInsets.symmetric(horizontal: -18),
+          border: InputBorder.none,
+          focusColor: Color(0xffFFFFFF),
+        ),
+        style: GoogleFonts.montserrat(
+          fontSize: 16,
+          fontWeight: FontWeight.w400,
+          letterSpacing: -0.2,
+          color: Color(0xffFFFFFF)
+        ),
+        maxLines: null,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:our_chat_app/components/custom_textfield.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Our Chat App',
-      home: const Placeholder(),
+      home: Scaffold(body: SafeArea(child: Center(child: CustomTextfield(),))),
     );
   }
 }
